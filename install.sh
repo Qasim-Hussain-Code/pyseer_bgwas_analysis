ce231bcdf2f9e8e671a3633fbc003a78f44ef418
Mr-Qasim-Hussain <qasimhussain470@yahoo.com>
2026-08-21 16:43:21 +0800

    update_install.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,11 @@ conda create -n pyseer
 # activate pyseer environment
 conda activate pyseer
 
+# install pyseer
+conda install pyseer
+
+# test pyseer installation
+# run unit tests
+pytest -v tests
+# test functions and outputs
+cd tests/ && bash run_test.sh && cd ../
