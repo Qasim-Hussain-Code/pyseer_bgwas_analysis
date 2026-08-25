5390f426280db8b27059e8a785c1cfddd8198676
Mr-Qasim-Hussain <qasimhussain470@yahoo.com>
2026-08-25 16:32:23 +0800

    update_analysis.sh

--- analysis.sh
+++ analysis.sh
@@ -7,9 +7,28 @@ eval "$(conda shell.bash hook)"
 # activate pyseer environment
 conda activate pyseer
 
-# get the data
-wget https://doi.org/10.6084/m9.figshare.7588832
+# fetch geonmes from ena
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR065/ERR065292/ERR065292_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR065/ERR065290/ERR065290_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR065/ERR065293/ERR065293_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR065/ERR065288/ERR065288_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR065/ERR065288/ERR065288_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR065/ERR065292/ERR065292_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR065/ERR065290/ERR065290_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR065/ERR065293/ERR065293_2.fastq.gz
 
-# extract the data
-unzip 7588832
+# create a directory for the downloaded files
+mkdir -p data
+
+# move the downloaded files to the data directory
+mv ERR065292_2.fastq.gz data/
+mv ERR065290_1.fastq.gz data/
+mv ERR065293_1.fastq.gz data/
+mv ERR065288_1.fastq.gz data/
+mv ERR065288_2.fastq.gz data/
+mv ERR065292_1.fastq.gz data/
+mv ERR065290_2.fastq.gz data/
+mv ERR065293_2.fastq.gz data/   
+
+
 
