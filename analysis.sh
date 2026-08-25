e116681e16243f7363620f242b0a51b80d2ad44c
Mr-Qasim-Hussain <qasimhussain470@yahoo.com>
2026-08-25 16:24:33 +0800

    update_analysis.sh

--- analysis.sh
+++ analysis.sh
@@ -1,1 +1,21 @@
+#!/bin/bash 
+set -e
+
+# initiate conda
+eval "$(conda shell.bash hook)"
+
+# activate pyseer environment
+conda activate pyseer
+
+# fetch geonmes from ena
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR065/ERR065292/ERR065292_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR065/ERR065290/ERR065290_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR065/ERR065293/ERR065293_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR065/ERR065288/ERR065288_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR065/ERR065288/ERR065288_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR065/ERR065292/ERR065292_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR065/ERR065290/ERR065290_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR065/ERR065293/ERR065293_2.fastq.gz
+
+
 
